1694b06698cca4c8d483f756d0f5363899c653f6
david-duartes <dduartes@gmail.com>
2023-02-12 21:45:43 -0300

    Add origin config

--- app.js
+++ app.js
@@ -7,6 +7,16 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.use('/feed', feedRoutes);
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+app.use('/feeds', feedRoutes);
 
 app.listen(8080);
